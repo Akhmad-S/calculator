b08d373069538f0f68a903800ba6ece194d96a5a
Akhmad-S <akhmadsuyunov95@gmail.com>
2022-07-26 16:04:15 +0500

    init calculator

---
+++ script.js
@@ -0,0 +1,48 @@
+let btnArray = document.getElementsByClassName("btn");
+
+function initBtn() {
+    for (let i = 0; i < btnArray.length; i++) {
+        btnArray[i].addEventListener('click', btnFunc);
+    }
+}
+
+result = document.getElementById("result");
+
+let btnFunc = function(event){
+    switch (event.target.value) {
+        case "+":
+        case "-":
+        case "*":
+        case "/":
+        case ".":
+            if (result.value[result.value.length - 1]!=event.target.value) {
+                result.value = result.value + event.target.value;
+            }
+            break;
+        case "C":
+            result.value = "";
+            break;
+        case "DEL":
+            result.value = result.value.slice(0, -1)
+            break;
+        case "=":
+            Calc();
+        break;
+        default:
+            result.value = result.value + event.target.value;    
+        }
+}
+
+initBtn();
+
+function Calc() {
+    try {
+        result.value = eval(result.value);
+    } catch (err) {
+        console.log(err);
+        if (err instanceof SyntaxError) {
+            alert(err.message);
+        }
+    }
+}
+
